0ceafe292f0db05a87c5dfc624a0d4d34470f703
CloudyLondon <jovialcty@gmail.com>
2022-02-11 21:52:55 -0800

    make it useful

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 const API_KEY = "5e465552b5b1cf92efd83e5e7d35aea2";
-const DateTime = luxon.DateTime;
 
 var currentCityEl = document.getElementById("currentCity");
 var currentDateEl = document.getElementById("currentDate");
@@ -23,22 +22,25 @@ var getForecast = function (cityName) {
     if (response.ok) {
       response.json().then((data) => {
         var currentCity = data.name;
+
         if (!searchHistory.includes(currentCity)) {
           searchHistory.push(currentCity);
           saveSearchHistory();
         }
+
+        currentCityEl.innerText = currentCity;
+        var displayDate = moment.unix(data.dt).format("MMMM Do YYYY");
+        currentDateEl.innerText = `${displayDate}`;
+
         fetch(
           `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&exclude=hourly&appid=${API_KEY}`
         ).then((response) =>
           response.json().then((data) => {
-            currentCityEl.innerText = currentCity;
-            currentDateEl.innerText = DateTime.fromSeconds(
-              data.current.dt
-            ).toLocaleString();
             currentWeatherIconEl.innerHTML = `<img src="https://openweathermap.org/img/w/${data.current.weather[0].icon}.png" />`;
             currentTempEl.innerHTML = `${data.current.temp}&deg;F`;
             currentWindEl.innerHTML = `${data.current.wind_speed} MPH`;
             currentHumidityEl.innerHTML = `${data.current.humidity}%`;
+
             var uvIndex = data.current.uvi;
             currentUvIndexEl.innerHTML = uvIndex;
             if (uvIndex < 5) {
@@ -49,12 +51,15 @@ var getForecast = function (cityName) {
               currentUvIndexEl.classList = "badge bg-danger";
             }
 
+            console.log();
+
             fiveDayForecastEl.innerHTML = "";
 
             var futureForecast = data.daily;
             for (var i = 1; i < 6; i++) {
               var forecastInfo = futureForecast[i];
-              var date = DateTime.fromSeconds(forecastInfo.dt).toLocaleString();
+              var dailyData = futureForecast[i];
+              var date = moment.unix(dailyData.dt).format("MMMM Do YYYY");
               var weatherIcon = `<img src="https://openweathermap.org/img/w/${forecastInfo.weather[0].icon}.png"`;
               var temp = forecastInfo.temp.day;
               var wind = forecastInfo.wind_speed;
@@ -63,7 +68,7 @@ var getForecast = function (cityName) {
               var futureForecastCardEl = document.createElement("div");
               futureForecastCardEl.classList = "card col-12 col-xl-2";
               var content = `<div class="card-body">
-            <h6 class="card-title">${date}</h4>
+            <h4 class="card-title">${date}</h4>
             <div class="card-text">
               <div>
                 ${weatherIcon}
@@ -79,7 +84,7 @@ var getForecast = function (cityName) {
               futureForecastCardEl.innerHTML = content;
               fiveDayForecastEl.appendChild(futureForecastCardEl);
 
-              console.log(date, temp, wind, humidity);
+              console.log(temp, wind, humidity);
             }
           })
         );
